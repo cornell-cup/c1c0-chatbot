c86a6c88e9c7b299242c921f646ab7892150b3be
Ganesh Rayavarapu <ganesh.rayavarapu@gmail.com>
2024-10-26 11:55:51 -0400

    change

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ from client.client import OpenAPI  # Client Interface
 from client.config import LABEL_THRESHOLD  # Configuration
 
 from labels.config import desc as config_desc, handler as config_handler  # Configuration Specifications
-from labels.general import desc as general_desc, handler as general_handler  # General Info Specifications
+from labels.general import recognize as general_recognize, handler as general_handler  # General Info Specifications
 from labels.movement import recognize as movement_recognize, handler as movement_handler  # Movement Specifications
 from labels.facial import recognize as facial_recognize, handler as facial_handler  # Facial Specifications
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # Initialzing response handlers and mapping
     mapping: Dict[str, Callable[[str], None]] = {
         # config_desc:      lambda msg: config_handler(client, msg),
-        general_desc:     lambda msg: general_handler(client, msg),
+        general_recognize:     lambda msg: general_handler(client, msg),
         movement_recognize: lambda msg: movement_handler(client, msg),
         facial_recognize:   lambda msg: facial_handler(client, msg)
     }
